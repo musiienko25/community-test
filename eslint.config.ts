e5fe8ee803a7b1de9e6bcbfd573937ec8960593b
musiienko25 <devmusiienko@gmail.com>
2025-01-21 17:38:24 +0200

    changed to ts

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,7 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(
+const config = tseslint.config(
   { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
@@ -26,3 +26,5 @@ export default tseslint.config(
     },
   },
 )
+
+export default config
